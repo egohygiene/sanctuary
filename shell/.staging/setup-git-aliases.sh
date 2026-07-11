cd7a0a1653b09ae3248f576665cdc91935c2a0e5
Alan Szmyt <szmyty@gmail.com>
2026-07-11 00:19:28 -0400

    Checkpoint

--- shell/.staging/setup-git-aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-git config --global alias.pullall '!git pull && git submodule update --init --recursive'
-git config --global alias.pushall '!git push && git submodule foreach --recursive git push'
-git config --global alias.reset-hard '!f() { git reset --hard; git clean -df ; }; f'
-git config --global alias.sync '!git submodule update --recursive --remote --merge'
