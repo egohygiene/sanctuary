06fec7050f31f7d4d6f8f69b26196a084be2a263
Alan Szmyt <szmyty@gmail.com>
2026-07-11 19:20:18 -0400

    Checkpoint

---
+++ templates/applications/react-vite/apps/egohygiene.io/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    tsconfigPaths: true,
+  },
+
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+});
+
